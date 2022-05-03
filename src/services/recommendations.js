5cbd52292b2e2b2d1eee860628016828a20fa030
Fábio Morato <fabiolmorato@dcc.ufmg.br>
2022-05-03 17:29:38 -0300

    feat: upvote and downvote service functionality

--- src/services/recommendations.js
+++ src/services/recommendations.js
@@ -9,3 +9,13 @@ export async function create(data) {
   const response = await api.post("/recommendations", data);
   return response.data;
 }
+
+export async function upvote(id) {
+  const response = await api.post(`/recommendations/${id}/upvote`);
+  return response.data;
+}
+
+export async function downvote(id) {
+  const response = await api.post(`/recommendations/${id}/downvote`);
+  return response.data;
+}
