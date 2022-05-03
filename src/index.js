7bcf322575f08f8db12df08401ca936e3db76eb2
Fábio Morato <fabiolmorato@dcc.ufmg.br>
2022-05-03 13:50:19 -0300

    feat: base css files

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import "./assets/css/reset.css";
+import "./assets/css/style.css";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
