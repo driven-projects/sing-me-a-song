573b3a0d45cc4843972a9b583204056eb9537ef9
Fábio Morato <fabiolmorato@dcc.ufmg.br>
2022-05-03 17:05:32 -0300

    feat: routes with lazy loading

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
-import { Suspense, lazy } from "react";
+import { Suspense, lazy, Component } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
 
-import Timeline from "./pages/Timeline";
+const Loading = () => (
+  <div>Loading...</div>
+  );
+  
+const LazyWrapper = (Component) => (props) => (
+  <Suspense fallback={<Loading />}>
+    <Component {...props} />
+  </Suspense>
+)
+
+const Timeline = LazyWrapper(lazy(() => import("./pages/Timeline")));
+const Home = LazyWrapper(lazy(() => import("./pages/Timeline/Home")));
 
 export default function App() {
   return (
-    <Timeline />
+    <Router>
+      <Routes>
+        <Route path="/" element={<Timeline />}>
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<div>Not found!</div>} />
+        </Route>
+      </Routes>
+    </Router>
   )
 }
