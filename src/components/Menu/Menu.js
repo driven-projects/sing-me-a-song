7ee10b06a79cc0a897d0d4c07d97075dff6d24be
Fábio Morato <fabiolmorato@dcc.ufmg.br>
2022-05-03 14:43:48 -0300

    feat: Menu component

---
+++ src/components/Menu/Menu.js
@@ -0,0 +1,45 @@
+import styled from "styled-components";
+
+import { FaHome } from "react-icons/fa";
+import { HiTrendingUp } from "react-icons/hi";
+import { FiShuffle } from "react-icons/fi";
+
+export default function Menu() {
+  return (
+    <StyledMenu>
+      <MenuItem>
+        <FaHome />
+        Home
+      </MenuItem>
+
+      <MenuItem>
+        <HiTrendingUp />
+        Top
+      </MenuItem>
+
+      <MenuItem>
+        <FiShuffle />
+        Random
+      </MenuItem>
+    </StyledMenu>
+  )
+}
+
+const StyledMenu = styled.div`
+  border-top: 1px solid #fff;
+  border-bottom: 1px solid #fff;
+  display: flex;
+  justify-content: space-between;
+  margin: 15px auto;
+  padding: 10px 14px;
+`;
+
+const MenuItem = styled.div`
+  display: flex;
+  align-items: center;
+  text-transform: lowercase;
+  gap: 15px;
+  font-size: 14px;
+  font-weight: 300;
+  cursor: pointer;
+`;
