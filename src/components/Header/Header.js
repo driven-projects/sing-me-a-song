a9c087b0f249c38e80e1fcd06a2b663d6dfc1f70
Fábio Morato <fabiolmorato@dcc.ufmg.br>
2022-05-03 14:43:39 -0300

    feat: Header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+import { VscPlayCircle } from "react-icons/vsc";
+
+export default function Header() {
+  return (
+    <StyledHeader>
+      <VscPlayCircle size="28px" color="#e90000" />
+      Sing me a Song
+    </StyledHeader>
+  );
+}
+
+const StyledHeader = styled.header`
+  display: flex;
+  align-items: center;
+  gap: 8px;
+  font-size: 20px;
+  text-transform: lowercase;
+`;
