d8f696a225a0d4e55fadd7718893fef9c1ff5cc1
Fábio Morato <fabiolmorato@dcc.ufmg.br>
2022-05-03 17:22:22 -0300

    fix: recommendation margin bottom

--- src/components/Recommendation/Recommendation.js
+++ src/components/Recommendation/Recommendation.js
@@ -1,12 +1,36 @@
 import styled from "styled-components";
 import ReactPlayer from "react-player";
+import { useEffect } from "react";
 
-export default function Recommendation({ name, youtubeLink, score }) {
+import useUpvoteRecommendation from "../../hooks/api/useUpvoteRecommendation";
+
+export default function Recommendation({ name, youtubeLink, score, id, onUpvote = () => 0, onDownvote = () => 0 }) {
+  const { upvoteRecommendation, errorUpvotingRecommendation } = useUpvoteRecommendation();
+
+  const handleUpvote = async () => {
+    await upvoteRecommendation(id);
+    onUpvote();
+  };
+
+  const handleDownvote = async () => {
+    await upvoteRecommendation(id);
+    onDownvote();
+  };
+
+  useEffect(() => {
+    if (errorUpvotingRecommendation) {
+      alert("Error upvoting recommendation!");
+    }
+  }, [errorUpvotingRecommendation]);
+
   return (
     <Container>
       <Row>{name}</Row>
       <ReactPlayer url={youtubeLink} width="100%" height="100%" />
-      <Row>{score}</Row>
+      <Row>
+        <button onClick={handleDownvote}>upvote</button>
+        {score}
+      </Row>
     </Container>
   );
 }
@@ -18,6 +42,7 @@ const Container = styled.article`
   padding: 15px 0;
   background-color: rgba(255, 255, 255, .1);
   border-radius: 4px;
+  margin-bottom: 15px;
 `;
 
 const Row = styled.div`
